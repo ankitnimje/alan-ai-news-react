6481d222de1e0e7d2a50563205778397f1dd5068
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-11-26 07:08:15 -0800

    Deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import wordsToNumbers from 'words-to-numbers';
 import NewsCards from './components/NewsCards/NewsCards';
 
 import useStyles from './styles.js';
@@ -15,12 +16,22 @@ const App = () => {
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command, articles }) => {
+            onCommand: ({ command, articles, number }) => {
                 if(command === 'newHeadlines') {
                     setNewsArticles(articles);
                     setActiveArticle(-1);
                 } else if(command === 'highlight') {
                     setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 )
+                } else if(command === 'open') {
+                    const parsedNumber = number.length > 2 ? wordsToNumbers(number, { fuzzy: true }) : number;
+                    const article = articles[parsedNumber - 1];
+
+                    if(parsedNumber > 20) {
+                        alanBtn().playText('Please try that again.')
+                    } else if(article) {
+                        window.open(article.url, '_blank');
+                        alanBtn().playText('Opening...');
+                    }
                 }
             }
         })
@@ -29,7 +40,7 @@ const App = () => {
     return (
         <div>
             <div className={classes.logoContainer}>
-                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="Allen Logo" />
+                <img src="https://miro.medium.com/max/3840/1*ChocH_eUxil5eaeXIsd3rw.png" className={classes.alanLogo} alt="Allen Logo" />
             </div>
 
             <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
 import classNames from 'classnames';
 
@@ -6,9 +6,21 @@ import useStyles from './styles';
 
 const NewsCard = ({ article: {description, publishedAt, source, title, url, urlToImage}, i, activeArticle }) => {
     const classes = useStyles();
+    const [elRefs, setElRefs] = useState([]);
+    const scrollToRef = (ref) => window.scroll(0, ref.current.offsetTop - 50);
+
+    useEffect(() => {
+        setElRefs((refs) => Array(20).fill().map((_, j) => refs[j] || createRef()));
+    }, []);
+
+    useEffect(() => {
+        if(i === activeArticle && elRefs[activeArticle]) {
+            scrollToRef(elRefs[activeArticle]);
+        }
+    }, [i, activeArticle, elRefs])
 
     return ( 
-        <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+        <Card ref={elRefs[i]} className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
             <CardActionArea href={url} target="_blank">
                 <CardMedia className={classes.media} image={urlToImage || 'https://images.unsplash.com/photo-1504465039710-0f49c0a47eb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'} />
                 <div className={classes.details}>
