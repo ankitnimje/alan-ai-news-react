e77a3ab1671792268775c91107f16ddca726f26a
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-11-26 04:33:03 -0800

    Update Article reading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
+import NewsCards from './components/NewsCards/NewsCards';
+
+import useStyles from './styles.js';
 
 const alanKey = process.env.REACT_APP_ALAN_KEY;
-const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
 
 const App = () => {
+
+    const [newsArticles, setNewsArticles] = useState([]);
+    const [activeArticle, setActiveArticle] = useState(-1);
+    const classes = useStyles();
 
     useEffect(() => {
         alanBtn({
             key: alanKey,
-            onCommand: ({ command }) => {
-                if(command === 'testCommand') {
-                    alert('this code was exucuted');
+            onCommand: ({ command, articles }) => {
+                if(command === 'newHeadlines') {
+                    setNewsArticles(articles);
+                    setActiveArticle(-1);
+                } else if(command === 'highlight') {
+                    setActiveArticle((prevActiveArticle) => prevActiveArticle + 1 )
                 }
             }
         })
-    });
+    }, []);
 
     return (
         <div>
-            <h1>ALAN AI News Application</h1>
+            <div className={classes.logoContainer}>
+                <img src="https://alan.app/voice/images/previews/preview.jpg" className={classes.alanLogo} alt="Allen Logo" />
+            </div>
+
+            <NewsCards articles={newsArticles} activeArticle={activeArticle}/>
         </div>
     );
 }
---
+++ src/components/NewsCard/NewsCard.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, CardActions, CardActionArea, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import classNames from 'classnames';
+
+import useStyles from './styles';
+
+const NewsCard = ({ article: {description, publishedAt, source, title, url, urlToImage}, i, activeArticle }) => {
+    const classes = useStyles();
+
+    return ( 
+        <Card className={classNames(classes.card, activeArticle === i ? classes.activeCard : null)}>
+            <CardActionArea href={url} target="_blank">
+                <CardMedia className={classes.media} image={urlToImage || 'https://images.unsplash.com/photo-1504465039710-0f49c0a47eb7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=675&q=80'} />
+                <div className={classes.details}>
+                    <Typography varient="body2" color="textSecondary" componet="h2">{(new Date(publishedAt)).toDateString()}</Typography>
+                    <Typography varient="body2" color="textSecondary" componet="h2">{source.name}</Typography>
+                </div>
+                <Typography className={classes.title} gutterBottom varient="h5">{title}</Typography>
+                <CardContent>
+                    <Typography variant="body2" color="textSecondary" component="p">{description}</Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions className={classes.cardActions}>
+                <Button size="small" color="primary">Learn More</Button>
+                <Typography varient="h5" color="textSecondary">{i+1}</Typography>
+            </CardActions>
+        </Card>
+     );
+}
+ 
+export default NewsCard;
