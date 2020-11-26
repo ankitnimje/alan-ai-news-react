96757ea2f36c28feed76a5af901e535704d7b9bd
Ankit Nimje Account <ankit.nimje93@gmail.com>
2020-11-26 00:51:01 -0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import alanBtn from '@alan-ai/alan-sdk-web';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = process.env.REACT_APP_ALAN_KEY;
+const newsApiKey = process.env.REACT_APP_NEWS_API_KEY;
+
+const App = () => {
+
+    useEffect(() => {
+        alanBtn({
+            key: alanKey,
+            onCommand: ({ command }) => {
+                if(command === 'testCommand') {
+                    alert('this code was exucuted');
+                }
+            }
+        })
+    });
+
+    return (
+        <div>
+            <h1>ALAN AI News Application</h1>
+        </div>
+    );
 }
 
 export default App;
